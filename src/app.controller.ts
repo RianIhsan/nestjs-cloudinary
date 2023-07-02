0271dfe61d014e9ec084e32a2a13419d00ec246a
RianIhsan <Rianwhatsup@gmail.com>
2023-07-02 23:28:55 +0700

    Finish

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadImage(@UploadedFile() file: Express.Multer.File): Promise<{ secure_url: string }> {
+    const uploadResult = await this.appService.uploadImageToCloudinary(file);
+    return { secure_url: uploadResult.secure_url };
+  }
+}
